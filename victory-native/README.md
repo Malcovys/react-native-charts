019ae60fdcbd7a0fc4a0a23e4bf1b41108ae2b30
malcovys <malcovys22.aps@gmail.com>
2025-05-20 09:53:37 +0300

    teste de victory native

---
+++ victory-native/README.md
@@ -0,0 +1,9 @@
+# React Native Victory Charts 📊
+
+Ce projet est une application React Native utilisant la bibliothèque [`victory-native`](https://formidable.com/open-source/victory/docs/native/) pour créer des graphiques interactifs et personnalisés.
+
+## Fonctionnalités
+
+- 📈 Création de graphiques en courbes, barres, secteurs, etc.
+- 🎨 Personnalisation des styles, couleurs et données.
+- 🖱️ Interaction avec les graphiques (animations, étiquettes dynamiques).
---
+++ victory-native/app/index.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { View } from "react-native";
+import { CartesianChart, Line, useChartPressState } from "victory-native";
+import { Circle, } from "@shopify/react-native-skia";
+import type { SharedValue } from "react-native-reanimated";
+
+
+export default function Index() {
+  const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } })
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CartesianChart
+        data={DATA}
+        xKey="day"
+        yKeys={["highTmp"]}
+        chartPressState={state}
+      >
+        {({ points }) => (
+          <>
+            <Line points={points.highTmp} color="red" strokeWidth={3} />
+            {isActive && (
+              <ToolTip x={state.x.position} y={state.y.highTmp.position} />
+            )}
+          </>
+        )}
+      </CartesianChart>
+    </View>
+  );
+}
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color="black" />;
+}
+
+const DATA = Array.from({ length: 31 }, (_, i) => ({
+  day: i,
+  highTmp: 40 + 30 * Math.random(),
+}));
